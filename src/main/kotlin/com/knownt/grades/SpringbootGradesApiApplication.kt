e347b0ad2bbe78a534e6b1832e5df5cf1c954730
Anderson R. Vanzo <anderson.vanzo01@gmail.com>
2023-10-31 22:16:14 -0300

    initial commit

---
+++ src/main/kotlin/com/knownt/grades/SpringbootGradesApiApplication.kt
@@ -0,0 +1,11 @@
+package com.knownt.grades
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootGradesApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringbootGradesApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/knownt/grades/SpringbootGradesApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.knownt.grades
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbootGradesApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
